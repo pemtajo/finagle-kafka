8e8608b74cd454c83c2e7b0e4adf862ae6fbd0f3
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2016-08-09 16:52:34 -0300

    Key and value decoder

--- src/main/scala/okapies/finagle/kafka/transport/KafkaConsumer.scala
+++ src/main/scala/okapies/finagle/kafka/transport/KafkaConsumer.scala
@@ -2,27 +2,23 @@ package okapies.finagle.kafka.transport
 
 import java.net.{InetSocketAddress, SocketAddress}
 import java.security.cert.Certificate
-import java.util
 import java.util.concurrent.Executors
 
 import com.twitter.finagle.Status
 import com.twitter.finagle.transport.Transport
-import com.twitter.util._
+import com.twitter.util.{Future, FuturePool, Promise, Time}
 import kafka.consumer.{ConsumerIterator, KafkaStream}
 import kafka.consumer.ConsumerConnector
 import kafka.message.MessageAndMetadata
-import kafka.producer.KeyedMessage
-import kafka.serializer.{DefaultDecoder, StringDecoder}
+import kafka.serializer.Decoder
 
-import scala.collection.JavaConversions._
-
-class KafkaConsumer(consumer:ConsumerConnector, topic:String)
- extends Transport[Any, MessageAndMetadata[String,String]] {
+class KafkaConsumer[K,V](consumer: ConsumerConnector, topic: String, keyDecoder: Decoder[K], valueDecoder: Decoder[V])
+ extends Transport[Any, MessageAndMetadata[K,V]] {
   val futurePool = FuturePool(Executors.newFixedThreadPool(1))
 
-  val msgs: KafkaStream[String, String] = consumer.createMessageStreams(Map(topic -> 1), new StringDecoder, new StringDecoder)(topic).head
+  val msgs: KafkaStream[K,V] = consumer.createMessageStreams(Map(topic -> 1), keyDecoder, valueDecoder)(topic).head
 
-  val iterator: ConsumerIterator[String, String] = msgs.iterator()
+  val iterator: ConsumerIterator[K,V] = msgs.iterator()
 
   override def write(req: Any): Future[Unit] = Future(req)
 
@@ -34,7 +30,7 @@ class KafkaConsumer(consumer:ConsumerConnector, topic:String)
 
   override def status: Status = Status.Open
 
-  override def read(): Future[MessageAndMetadata[String, String]] =
+  override def read(): Future[MessageAndMetadata[K, V]] =
     futurePool {
       iterator.next()
     }
--- src/main/scala/okapies/finagle/kafka/transport/KafkaServer.scala
+++ src/main/scala/okapies/finagle/kafka/transport/KafkaServer.scala
@@ -14,6 +14,7 @@ import com.twitter.util.{Closable, Future}
 import kafka.consumer.{Consumer, ConsumerConfig, ConsumerConnector}
 import kafka.message.MessageAndMetadata
 import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
+import kafka.serializer.{Decoder, DefaultDecoder, StringDecoder}
 
 
 case class Config(prop:Properties) {
@@ -25,35 +26,66 @@ object Config {
   implicit val param = Stack.Param(Config(new Properties()))
 }
 
-case class KafkaServer(stack: Stack[ServiceFactory[MessageAndMetadata[String,String], Any]] = StackServer.newStack[MessageAndMetadata[String,String], Any],
-                       params: Params = StackServer.defaultParams + Config(new Properties)) extends StdStackServer[MessageAndMetadata[String,String],Any,KafkaServer] {
+case class KeyDecoder(decoder:Decoder[_]) {
+  def mk(): (KeyDecoder, Stack.Param[KeyDecoder]) =
+    (this, KeyDecoder.param)
+}
+
+object KeyDecoder {
+  implicit val param = Stack.Param(KeyDecoder(new DefaultDecoder))
+}
+
+case class ValueDecoder(decoder:Decoder[_]) {
+  def mk(): (ValueDecoder, Stack.Param[ValueDecoder]) =
+    (this, ValueDecoder.param)
+}
+
+object ValueDecoder {
+  implicit val param = Stack.Param(ValueDecoder(new DefaultDecoder))
+}
+
+
+
+case class KafkaServer[K,V](stack: Stack[ServiceFactory[MessageAndMetadata[K,V], Any]] = StackServer.newStack[MessageAndMetadata[K,V], Any],
+                       params: Params = StackServer.defaultParams + Config(new Properties) + KeyDecoder(new DefaultDecoder) + ValueDecoder(new DefaultDecoder))
+  extends StdStackServer[MessageAndMetadata[K,V],Any,KafkaServer[K,V]] {
   override type In = Any
-  override type Out = MessageAndMetadata[String,String]
+  override type Out = MessageAndMetadata[K,V]
 
   override protected def newListener(): Listener[In, Out] = new Listener[In,Out] {
     override def listen(addr: SocketAddress)(serveTransport: (Transport[In, Out]) => Unit): ListeningServer = {
       val Config(props) = params[Config]
+      val KeyDecoder(keyDecoder:Decoder[K]) = params[KeyDecoder]
+      val ValueDecoder(valueDecoder:Decoder[V]) = params[ValueDecoder]
       props.setProperty("zookeeper.connect", addr match {
         case a:InetSocketAddress => s"${a.getHostName}:${a.getPort}"
       })
       val consumer: ConsumerConnector = Consumer.create(new ConsumerConfig(props))
-      serveTransport(new KafkaConsumer(consumer, "ZUEIRA"))
+      serveTransport(new KafkaConsumer(consumer, "ZUEIRA", keyDecoder, valueDecoder))
       NullServer
     }
   }
 
-  override protected def newDispatcher(transport: Transport[In, Out], service: Service[MessageAndMetadata[String,String], Any]): Closable =
+  override protected def newDispatcher(transport: Transport[In, Out], service: Service[MessageAndMetadata[K,V], Any]): Closable =
     new SerialServerDispatcher(transport, service)
 
-  override protected def copy1(stack: Stack[ServiceFactory[MessageAndMetadata[String, String], Any]] = this.stack, params: Params = this.params): KafkaServer = copy(stack, params)
+  override protected def copy1(stack: Stack[ServiceFactory[MessageAndMetadata[K, V], Any]] = this.stack, params: Params = this.params): KafkaServer[K,V] = copy(stack, params)
 
-  def withProperties(props: Map[String, String]): KafkaServer = {
+  def withProperties(props: Map[String, String]): KafkaServer[K,V] = {
     val Config(properties) = params[Config]
     props.foreach { case (k,v) =>  properties.setProperty(k,v)}
     this
   }
 
-  def withGroup(group: String): KafkaServer = {
+  def withKeyDecoder[NK](decoder:Decoder[NK]):KafkaServer[NK,V] = {
+    configured(KeyDecoder(decoder)).asInstanceOf[KafkaServer[NK,V]]
+  }
+
+  def withValueDecoder[NV](decoder:Decoder[NV]):KafkaServer[K,NV] = {
+    configured(ValueDecoder(decoder)).asInstanceOf[KafkaServer[K,NV]]
+  }
+
+  def withGroup(group: String): KafkaServer[K,V] = {
     params[Config].prop.setProperty("group.id", group)
     this
   }
@@ -103,8 +135,8 @@ object KafkaServer {
     .build()
 
   def builder = ServerBuilder()
-    .stack(server)
+    .stack(KafkaServer().withGroup("DOUGH").withKeyDecoder(new StringDecoder).withValueDecoder(new StringDecoder))
     .name("server")
       .bindTo(new InetSocketAddress("localhost", 2181))
-    .build(Service.mk[MessageAndMetadata[String,String], Any](x => Future(println(x))))
+    .build(Service.mk[MessageAndMetadata[String,String], Any](x => Future(println(x.key -> x.message))))
 }
